a40a9fc25911c38d55994636979921d268ccc8b0
Bala415 <kyaswanth722@gmail.com>
2024-04-23 00:13:30 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<RcbHeader Title="RCB SQUAD TRACKER"/>
+  <div id="app">
+    <PlayerList :players=players :loading="loading" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlayerList from './components/PlayerList.vue';
+import RcbHeader from './components/RcbHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    RcbHeader,
+    PlayerList,
+  },
+  data() {
+    return {
+      players: [],
+      loading: true,
+      error: null,
+    };
+  },
+  methods: {
+    async fetchplayers() {
+      try {
+        const res = await fetch('https://rcb-squad-tracker.onrender.com/api');
+        const data = await res.json();
+        console.log("Data: ",data);
+        return data[0].RCBSquad;
+      } catch (error) {
+        this.error = 'Failed to fetch data';
+        console.error(error);
+      }
+    },
+  },
+  async created() {
+    try {
+      this.players = await this.fetchplayers();
+    } catch (error) {
+      this.error = 'Failed to load players';
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
 </style>
